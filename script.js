d9ad5de484e7bfdaf7ddaaac3c51f172560660ed
JAY PALANDE <jaypalande1425@gmail.com>
2025-06-27 14:35:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function insert_Row() {
-    //Write your code here
-  
-  
+    // Get the table by its id
+    let table = document.getElementById("sampleTable");
+
+    // Insert a new row at the top (index 0)
+    let newRow = table.insertRow(0);
+
+    // Insert two cells in the new row
+    let cell1 = newRow.insertCell(0);
+    let cell2 = newRow.insertCell(1);
+
+    // Add text to the new cells
+    cell1.innerHTML = "New row cell1";
+    cell2.innerHTML = "New row cell2";
 }
